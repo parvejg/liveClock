63f48101760a6733408f76f68fc50a82b2248119
parvejg <parvezpg321@gmail.com>
2025-08-12 00:19:30 +0530

    complete logic work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [time, setTime] = useState(0);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Live Clock + Counter</h1>
+      <h2>{time.toLocaleString()}</h2>
+      <h3>Counter: {count}</h3>
+      <div>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count + 1)}>Decrement</button>
+        <button onClick={() => setCount(0)}>Rest</button>
+      </div>
     </div>
   );
 }
