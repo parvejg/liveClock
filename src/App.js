189e7adadef195c92c8cf221c1ef26bd81f6dd4b
parvejg <parvezpg321@gmail.com>
2025-08-12 00:40:01 +0530

    complete time work

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [time, setTime] = useState(0);
+  const [time, setTime] = useState(new Date());
+
   useEffect(() => {
     const timer = setInterval(() => {
       setTime(new Date());
@@ -14,11 +15,17 @@ function App() {
   return (
     <div className="App">
       <h1>Live Clock + Counter</h1>
-      <h2>{time.toLocaleString()}</h2>
+      <h2>{time.toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: true
+      })}</h2>
+      <h2>{time.toLocaleDateString()}</h2>
+
       <h3>Counter: {count}</h3>
       <div>
         <button onClick={() => setCount(count + 1)}>Increment</button>
-        <button onClick={() => setCount(count + 1)}>Decrement</button>
+        <button onClick={() => count >0 &&  setCount(count - 1)}>Decrement</button>
         <button onClick={() => setCount(0)}>Rest</button>
       </div>
     </div>
